62a8e5d5f4ec7dd2bace30d32362a9e2f9f3eabf
Matt Rosanio and Neil Larion <mmrosani@syr.edu>
2016-01-25 08:37:57 -0800

    add test and function to Contact constructor to return full name

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ function Contact(firstName,lastName){
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
+  this.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,9 @@ describe('Contact',function(){
     expect(testContact.lastName).to.equal("Moreno");
     expect(testContact.addresses).to.eql([]);
   });
+
+  it("will add the fullName method to all contacts", function() {
+    var testContact = new Contact("Sherlock","Holmes");
+    expect(testContact.fullName()).to.equal("Sherlock Holmes");
+  });
 });
