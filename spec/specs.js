125ed500d93b58e61fff648f1b9be5b4b3422505
Matt Rosanio and Neil Larion <mmrosani@syr.edu>
2016-01-25 08:29:30 -0800

    Set up first unit test to test Contact constructor and function to pass test

---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+describe('Contact',function(){
+  it("Create a new Contact with the given properties", function(){
+    var testContact = new Contact("Rita","Moreno");
+    expect(testContact.firstName).to.equal("Rita");
+    expect(testContact.lastName).to.equal("Moreno");
+    expect(testContact.addresses).to.eql([]);
+  });
+});
